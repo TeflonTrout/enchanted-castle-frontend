a7bad669588aa731f4589a5eee1ba8635b260f6c
TeflonTrout <jt@joy.world>
2023-12-18 14:48:44 -0500

    Udpated individual card route to handle links to /search route

---
+++ src/components/DetailedCard/DetailedCard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let imgUrl:string;
+    export let name:string;
+</script>
+
+<div class="detailedCard">
+    <img src={imgUrl} alt="card preview">
+    <h4>{name}</h4>
+</div>
+
+<style>
+    .detailedCard {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .detailedCard img {
+        max-height: 250px;
+    }
+
+    .detailedCard h4 {
+        width: 100%;
+        text-align: center;
+    }
+</style>
---
+++ src/utils/franchiseArray.js
@@ -0,0 +1,127 @@
+export const franchiseMap = [
+    {
+      "id": 1,
+      "franchise_name": "Mickey Mouse",
+      "franchise_code": "MM"
+    },
+    {
+      "id": 2,
+      "franchise_name": "Lilo and Stitch",
+      "franchise_code": "LS"
+    },
+    {
+      "id": 3,
+      "franchise_name": "Frozen",
+      "franchise_code": "FRZ"
+    },
+    {
+      "id": 4,
+      "franchise_name": "101 Dalmatians",
+      "franchise_code": "101D"
+    },
+    {
+      "id": 5,
+      "franchise_name": "Sleeping Beauty",
+      "franchise_code": "SB"
+    },
+    {
+      "id": 6,
+      "franchise_name": "Robin Hood",
+      "franchise_code": "RH"
+    },
+    {
+      "id": 7,
+      "franchise_name": "Peter Pan",
+      "franchise_code": "PP"
+    },
+    {
+      "id": 8,
+      "franchise_name": "Mulan",
+      "franchise_code": "MLN"
+    },
+    {
+      "id": 9,
+      "franchise_name": "Hercules",
+      "franchise_code": "HRC"
+    },
+    {
+      "id": 10,
+      "franchise_name": "Aladdin",
+      "franchise_code": "ALD"
+    },
+    {
+      "id": 11,
+      "franchise_name": "Moana",
+      "franchise_code": "MNA"
+    },
+    {
+      "id": 12,
+      "franchise_name": "The Lion King",
+      "franchise_code": "TLK"
+    },
+    {
+      "id": 13,
+      "franchise_name": "Tangled",
+      "franchise_code": "TGL"
+    },
+    {
+      "id": 14,
+      "franchise_name": "Beauty and the Beast",
+      "franchise_code": "BATB"
+    },
+    {
+      "id": 15,
+      "franchise_name": "Snow White",
+      "franchise_code": "SW"
+    },
+    {
+      "id": 16,
+      "franchise_name": "Alice in Wonderland",
+      "franchise_code": "AIW"
+    },
+    {
+      "id": 17,
+      "franchise_name": "Winnie the Pooh",
+      "franchise_code": "WTP"
+    },
+    {
+      "id": 18,
+      "franchise_name": "Cinderella",
+      "franchise_code": "CND"
+    },
+    {
+      "id": 19,
+      "franchise_name": "Fantasia",
+      "franchise_code": "FNT"
+    },
+    {
+      "id": 20,
+      "franchise_name": "The Emperor's New Groove",
+      "franchise_code": "ENG"
+    },
+    {
+      "id": 21,
+      "franchise_name": "The Little Mermaid",
+      "franchise_code": "TLM"
+    },
+    {
+      "id": 22,
+      "franchise_name": "The Princess and the Frog",
+      "franchise_code": "PATF"
+    },
+    {
+      "id": 23,
+      "franchise_name": "The Sword in the Stone",
+      "franchise_code": "TSITS"
+    },
+    {
+      "id": 24,
+      "franchise_name": "Treasure Planet",
+      "franchise_code": "TP"
+    },
+    {
+      "id": 0,
+      "franchise_name": "Unknown",
+      "franchise_code": "UNK"
+    }
+  ]
