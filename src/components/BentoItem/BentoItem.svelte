36e25a315601c0b031ec615f69765572544627ad
TeflonTrout <jt@joy.world>
2024-01-09 14:54:16 -0500

    Updated home page with bento box UI

---
+++ src/components/BentoItem/BentoItem.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    export let text:string
+    export let width:number | null = null
+    export let height:number | null = null
+    export let link:string
+    export let external:boolean = false
+    export let wallpaper:any = null
+</script>
+
+{#if external}
+    <a class="item" href={link} target="_blank" rel="noopener noreferrer" style={`grid-area: span ${height} / span ${width};`}>
+        <h2>{text}</h2>
+    </a>
+{:else}
+    <a class="item" href={link} style={`grid-area: span ${height} / span ${width};`}>    
+        <h2>{text}</h2>
+    </a>
+{/if} 
+    
+<style>
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    .item {
+        border: 2px solid black;
+        background-color: #f5f5f5;
+        border-radius: 0.5rem;
+        padding: 1rem;
+        transition: .2s ease-in-out;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        text-align: center;
+        background-color: #8EC5FC;
+        background-image: linear-gradient(45deg, #8EC5FC 0%, #E0C3FC 100%);
+    }
+
+    .item:hover {
+        transform: scale(1.05);
+        transition: .2s ease-in-out;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,30 @@
+<script>
+  import BentoItem from "../components/BentoItem/BentoItem.svelte";
+</script>
 <div class="home">
-    <h1>Enchanted Castle</h1>
-    <div class="hero">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, expedita nihil. Pariatur voluptatibus aut dolor quasi tempore sunt dignissimos quibusdam.</p>
+    <div class="bentoBox">
+        <div class="hero" style="grid-area: span 1 / span 6;">
+            <h1>Welcome to Enchanted Castle!</h1>
+            <h4>Your home for all things Lorcana!</h4>    
+        </div>
+        <BentoItem text="View Cards" link="/all" height={2} width={3}/>
+        <div class="container" style="grid-area: span 2 / span 3;">
+            <BentoItem text="About Us" link="/about"/>
+            <BentoItem text="API Docs" link="/docs"/>
+        </div>
+        <!-- BREAK -->
+        <div class="container" style="grid-area: span 3 / span 2;">
+            <BentoItem text="View Cards By Set" link="/sets"/>
+            <BentoItem text="Join Our Discord" link="" external/>
+            <BentoItem text="References" link="/" />
+        </div>
+        <BentoItem text="Rise of the Floodborn" link="/RFB" height={3} width={2} />
+        <BentoItem text="The First Chapter" link="/TFC" height={3} width={2} />
+        <!-- BREAK -->
+        <BentoItem text="Contact Us" link="/contact" height={1} width={3} />
+        <BentoItem text="X" link="/" external height={1} width={1} />
+        <BentoItem text="Card Search" link="/search" height={1} width={1} />
+        <BentoItem text="Report Issues" link="/" height={1} width={1} />
     </div>
 </div>
 
@@ -14,5 +37,42 @@
         width: 100%;
         min-height: calc(100vh - 160px);
         padding: 10px 20px;
+    }
+
+    .hero {
+        display: flex;
+        flex-direction: column;
+        border-radius: 0.5rem;
+        padding: 1rem;
+        transition: .2s ease-in-out;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+    }
+
+    .bentoBox {
+        display: grid;
+        width: 90%;
+        margin-top: 20px;
+        grid-column-gap: 2rem;
+        grid-row-gap: 2rem;
+        grid-template-columns: repeat(6, 1fr);
+        grid-template-rows: repeat(8, 100px);
+    }
+
+    .container {
+        display: grid;
+        grid-column-gap: 2rem;
+        grid-row-gap: 2rem;
+        grid-template-columns: 1fr;
+        grid-template-rows: repeat(2, 100px);
+    }
+
+    /* Responsive adjustments for larger screens */
+    @media (max-width: 768px) {
+        .bentoBox {
+            grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+            grid-template-rows: repeat(8, 75px);
+        }
     }
 </style>
